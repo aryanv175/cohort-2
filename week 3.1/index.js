a5e01742783b0e9a83a4707749e1524aec924e2b
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-02-05 22:48:55 -0500

    first exercise.

--- week 3.1/index.js
+++ week 3.1/index.js
@@ -1,1 +1,27 @@
-console.log("Halo");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get('/health-checkup', (req, res) => {
+    const kidenyId = req.query.kidenyId;
+    const username = req.headers.username;
+    const pass = req.headers.pass;
+
+    if (username !== "admin" || pass !== "123"){
+        res.status(403).json({
+            msg: "user does not exist or wrong credentials."
+        })
+    }
+
+    if (kidenyId !== 1 || kidenyId !== 2){
+        res.status(403).json({
+            msg: "invalid kidney credentials."
+        })
+    }
+
+    res.json("hey there");
+})
+
+app.listen(port, ()=> {
+    console.log(`listening on port ${port}`);
+})

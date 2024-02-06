d767b14c9c930c9c986cd31d9a0a17a458d00b25
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-02-05 23:03:25 -0500

    updates

--- week 3.1/index.js
+++ week 3.1/index.js
@@ -3,17 +3,17 @@ const app = express();
 const port = 3000;
 
 app.get('/health-checkup', (req, res) => {
-    const kidenyId = req.query.kidenyId;
+    const kidneyId = req.query.kidneyId;
     const username = req.headers.username;
     const pass = req.headers.pass;
 
-    if (username !== "admin" || pass !== "123"){
+    if (username !== "admin" || pass !== "pass"){
         res.status(403).json({
             msg: "user does not exist or wrong credentials."
         })
     }
 
-    if (kidenyId !== 1 || kidenyId !== 2){
+    if (kidneyId != 1 && kidneyId != 2){
         res.status(403).json({
             msg: "invalid kidney credentials."
         })

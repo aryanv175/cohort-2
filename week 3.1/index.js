a6a631e76f49540106fa04e167d8b3883a6826fe
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-02-05 23:29:09 -0500

    implemented using middlewares.

--- week 3.1/index.js
+++ week 3.1/index.js
@@ -25,6 +25,39 @@ app.get('/health-checkup', (req, res) => {
 })
 */
 
+// using middlewares
+
+function userAuth (req, res, next){
+    const username = req.headers.username;
+    const pass = req.headers.pass;
+
+    if (username !== "admin" || pass !== "pass"){
+        res.status(403).json({
+            msg: "user does not exist or wrong credentials."
+        })
+    } else {
+        next();
+    }
+}
+
+function kidneyAuth (req, res, next) {
+    const kidneyId = req.query.kidneyId;
+
+    if (kidneyId != 1 && kidneyId != 2){
+        res.status(403).json({
+            msg: "invalid kidney credentials."
+        })
+    } else {
+        next();
+    }
+}
+
+app.get('/healthCheckup', userAuth, kidneyAuth, (req, res) => {
+    res.status(200).json({
+        msg: "Successfully authenticated. Your kidneys are healthy"
+    });
+})
+
 app.listen(port, ()=> {
     console.log(`listening on port ${port}`);
 })

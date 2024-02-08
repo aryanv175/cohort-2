db17d52f2932adbae0cc2b70fb3009c9ba1b7c04
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-02-08 00:02:22 -0500

    start using databases

--- week 3.2/main.js
+++ week 3.2/main.js
@@ -1,8 +1,11 @@
+// auth basics
+/*
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const jwtPassword = "123456";
 
 const app = express();
+app.use(express.json())
 
 const ALL_USERS = [
   {
@@ -23,11 +26,71 @@ const ALL_USERS = [
 ];
 
 function userExists(username, password) {
-  // write logic to return true or false if this user exists
-  // in ALL_USERS array
+  // go over the array to see if that user exists
+  for (let i =0; i<ALL_USERS.length; i++){
+    if(ALL_USERS[i].username == username && ALL_USERS[i].password == password){
+        return true
+    }
+  }
+
+  return false
 }
 
 app.post("/signin", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!userExists(username, password)) {
+    return res.status(403).json({
+      msg: "User doesnt exist in our in memory db",
+    });
+  }
+
+  var token = jwt.sign({ username: username }, "shhhhh");
+  return res.json({
+    token,
+  });
+});
+
+app.get("/users", function (req, res) {
+    const token = req.headers.authorization;
+    const decoded = jwt.verify(token, jwtPassword);
+    const username = decoded.username;
+    // return a list of users other than this username
+    res.json({
+        users: ALL_USERS
+    })
+
+});
+
+app.listen(3000)
+
+*/
+
+// start using databases
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const jwtPassword = "123456";
+
+mongoose.connect(
+  "your_mongo_url",
+);
+
+const User = mongoose.model("User", {
+  name: String,
+  username: String,
+  pasword: String,
+});
+
+const app = express();
+app.use(express.json());
+
+function userExists(username, password) {
+  // should check in the database
+}
+
+app.post("/signin", async function (req, res) {
   const username = req.body.username;
   const password = req.body.password;
 
@@ -48,7 +111,7 @@ app.get("/users", function (req, res) {
   try {
     const decoded = jwt.verify(token, jwtPassword);
     const username = decoded.username;
-    // return a list of users other than this username
+    // return a list of users other than this username from the database
   } catch (err) {
     return res.status(403).json({
       msg: "Invalid token",
@@ -56,4 +119,4 @@ app.get("/users", function (req, res) {
   }
 });
 
-app.listen(3000)
+app.listen(3000);

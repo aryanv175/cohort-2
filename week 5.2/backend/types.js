b309848b6a0bff56bfc256811e749529e85975f1
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-02-23 08:29:13 -0500

    schemas made

--- week 5.2/backend/types.js
+++ week 5.2/backend/types.js
@@ -1,2 +1,35 @@
 const z = require("zod");
 
+/*
+for the post /todo endpoint we need the body to be
+something like this.
+{
+    title: string,
+    description: string
+}
+
+for the get /todos they are just asking for all the todos
+so no schema needs to be followed, since there is no imput
+
+for the put request /completed, the user will provide
+in the body, the id of the todo they want to mark as
+completed.
+{
+    id: string
+}
+*/
+
+const createTodo = z.object({
+    title: z.string(),
+    desccription: z.string()
+});
+
+const updateTodo = z.object({
+    id: z.string()
+});
+
+
+module.exports= {
+    createTodo: createTodo,
+    updateTodo: updateTodo
+}

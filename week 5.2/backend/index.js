c8e69fb9b7694de4526524994888fa9653d425db
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-02-24 03:01:01 -0500

    connecting frontend and backend

--- week 5.2/backend/index.js
+++ week 5.2/backend/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const { createTodo, updateTodo } = require("./types");
 const { Todo } = require("./db");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
 app.use(express.json())
+app.use(cors({
+    origin: "http:localhost/5173"
+}))
 
 app.post('/todo', async (req, res)=>{
     // get the body from the req
--- week 5.2/frontend/src/App.jsx
+++ week 5.2/frontend/src/App.jsx
@@ -8,16 +8,17 @@ import { Todos } from './componenets/Todos'
 function App() {
   const [todos, setTodos] = useState([]);
 
+  // used to fetch the existitng todos from the backend
+  fetch("https://localhost:300/todos")
+    .then(async function(res){
+      const json = await res.json();
+      setTodos(json.todos);
+    })
+
   return (
     <div>
       <CreateTodo/>
-      <Todos todos={[
-        {
-          title: "sjadhfsdhf",
-          description: "dhfghdfgdrhg",
-          completed: false
-        }
-      ]}></Todos>
+      <Todos todos={todos}></Todos>
     </div>
   )
 }

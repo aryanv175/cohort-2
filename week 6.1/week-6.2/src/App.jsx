5908cb6cd32b514da58620051e30c7fde8156361
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-02-25 04:50:53 -0500

    used react.memo

--- week 6.1/week-6.2/src/App.jsx
+++ week 6.1/week-6.2/src/App.jsx
@@ -1,3 +1,7 @@
+// pushing down state as much as possible
+
+/*
+
 import React from "react"
 import { useState } from "react"
 
@@ -32,5 +36,41 @@ function Header({title}){
     My name is {title}
   </div>
 }
+
+export default App
+
+
+*/
+
+// using react.memo
+// What is react memo? Memo lets you skip re-rendering 
+// of a component if its props are unchanged
+import React from "react"
+import { useState } from "react"
+
+function App() {
+  const [title, setTitle] = useState("title1")
+
+  function updateTitle (){
+    setTitle(Math.random());
+  }
+
+  return (
+    <div>
+      <button onClick={updateTitle}>Click me to change the title</button>
+      <Header title={title}></Header>
+      <Header title="title2"></Header>
+      <Header title="title3"></Header>
+      <Header title="title4"></Header>
+      <Header title="title5"></Header>
+    </div>
+  )
+}
+
+const Header = React.memo(function Header({title}){
+  return <div>
+    My name is {title}
+  </div>
+})
 
 export default App

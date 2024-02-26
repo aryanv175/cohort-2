d2189246bbf1fc08bc5cea81d6b01684de32f4f2
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-02-25 23:29:55 -0500

    understanding keys in react

--- week 6.1/Todo-project-week6.1/src/App.jsx
+++ week 6.1/Todo-project-week6.1/src/App.jsx
@@ -1,35 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+let counter = 4;
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function App() {
+  const [todos, setTodo] = useState([{
+    id: 1,
+    title: "todo1",
+    description: "description1"
+  },{
+    id: 2,
+    title: "todo2",
+    description: "description2"
+  },{
+    id: 3,
+    title: "todo3",
+    description: "description3"
+  }])
+
+  function addTodo(){
+    setTodo([...todos, {
+    id: counter++,
+    title: Math.random(),
+    description: Math.random()
+    }])
+  }
+
+  return (<div>
+    <button onClick={addTodo}>Add a new todo</button>
+    {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description}/>)}
+  </div>)
+}
+
+function Todo ({title, description}) {
+  return(<div>
+    <h1>{title}</h1>
+    <h2>{description}</h2>
+  </div>)
 }
 
 export default App

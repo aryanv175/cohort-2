dcef58bbc37d0a7bdef4989b1ff9a7dbf7ab0e2c
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-02-27 01:33:35 -0500

    useEffect

--- week 6.2/practice/src/App.jsx
+++ week 6.2/practice/src/App.jsx
@@ -1,16 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import axios from "axios"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodo] = useState([])
+  // we will try to use axios instead of fetch
+  useEffect(()=> {
+    axios.get("https://sum-server.100xdevs.com/todos")
+      .then(function(response){
+        setTodo(response.data.todos)
+      })
+  }, [])
 
   return (
     <div>
-      
+      {todos.map(todo => <Todo title={todo.title} description ={todo.description}/>)}
     </div>
   )
+}
+
+function Todo ({title, description}){
+  return <div>
+    <h1>{title}</h1>
+    <h2>{description}</h2>
+  </div>
 }
 
 export default App

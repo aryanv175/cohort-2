4e0f47fafc796c6dc2cd19dbc3ed96217951f04b
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-02-27 02:43:14 -0500

    useEffect more practice

--- week 6.2/practice/src/App.jsx
+++ week 6.2/practice/src/App.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react'
 import axios from "axios"
 import './App.css'
 
+// first example of useEffect
+/*
 function App() {
   const [todos, setTodo] = useState([])
   // we will try to use axios instead of fetch
@@ -24,6 +26,47 @@ function Todo ({title, description}){
     <h1>{title}</h1>
     <h2>{description}</h2>
   </div>
+}
+*/
+
+function App (){
+
+  const [id, setId] = useState(1)
+
+  return <div>
+    <button onClick={() => changeId(1)}>1</button>
+    <button onClick={() => changeId(2)}>2</button>
+    <button onClick={() => changeId(3)}>3</button>
+    <button onClick={() => changeId(4)}>4</button>
+    <Todo id={id}/>
+  </div>
+
+
+  function changeId(newId) {
+    setId(newId)
+  }
+
+}
+
+function Todo ({id}) {
+
+  const [todo, setTodo] = useState({});
+
+  useEffect(() => {
+    axios.get("https://sum-server.100xdevs.com/todo?id="+ id)
+      .then(function(response){
+        setTodo(response.data.todo)
+      })
+  }, [id])
+
+  return <div>
+    <h1>
+      {todo.title}
+    </h1>
+    <h2>
+    {todo.description}
+    </h2>
+  </div> 
 }
 
 export default App

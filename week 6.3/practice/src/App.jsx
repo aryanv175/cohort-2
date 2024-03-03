52cab5bac494e0b36c0944f061152a8fe7d2e16f
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-03-03 04:13:52 -0500

    prac

--- week 6.3/practice/src/App.jsx
+++ week 6.3/practice/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,24 +6,33 @@ import './App.css'
 
 function App() {
   const [exchangeData, setExchangeData] = useState({});
+  const [exchange2Data, setExchange2Data] = useState({});
   const [bankData, setBankData] = useState({});
   
   useEffect(()=> {
-    setTimeout(() => {
-      setBankData({ income: 100 
+      setExchangeData({ income: 100 
       });
-    }, 3000);
+  }, [])
+
+  useEffect(()=> {
+    setExchange2Data({ income: 100 
+    });
   }, [])
 
   useEffect(()=> {
     setTimeout(() => {
-      setExchangeData({
+      setBankData({
         returns: 100
       });
-    }, 1000);
+    }, 5000);
   }, [])
 
-  const incomeTax = (bankData.income + exchangeData.returns) * 0.3;
+  const cryptoReturns = useMemo(() => {
+    return exchangeData.income + exchange2Data.income;
+  }, [exchangeData, exchange2Data])
+
+
+  const incomeTax = (bankData.returns + cryptoReturns) * 0.3;
 
   return (
     <div>

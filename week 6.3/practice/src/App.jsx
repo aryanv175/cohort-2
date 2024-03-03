cfa9b5790978ff91745de72a8bf377dd0d7058c3
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-03-03 03:45:54 -0500

    useEffect recap

--- week 6.3/practice/src/App.jsx
+++ week 6.3/practice/src/App.jsx
@@ -1,14 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [exchangeData, setExchangeData] = useState({});
+  const [bankData, setBankData] = useState({});
+  
+  useEffect(()=> {
+    setTimeout(() => {
+      setBankData({ income: 100 
+      });
+    }, 3000);
+  }, [])
+
+  useEffect(()=> {
+    setTimeout(() => {
+      setExchangeData({
+        returns: 100
+      });
+    }, 1000);
+  }, [])
+
+  const incomeTax = (bankData.income + exchangeData.returns) * 0.3;
 
   return (
     <div>
-      <button onClick={() => {setCount(count+1)}}>The count is {count}</button>
+        hi there, your income tax returns are {incomeTax}
     </div>
   )
 }

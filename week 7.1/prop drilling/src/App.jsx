b2bca8168f1f9d5db0f358d987c9cf40dcd8c30d
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-04-03 02:08:35 -0400

    setup prop drilling

--- week 7.1/prop drilling/src/App.jsx
+++ week 7.1/prop drilling/src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div>
+      <Count/>
+      <Buttons/>
     </div>
   )
+}
+
+function Count() {
+
+}
+
+function Buttons() {
+
 }
 
 export default App

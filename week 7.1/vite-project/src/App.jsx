db28ccf8f14845164d26f592721dc094dcfaf2ef
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-04-03 00:49:27 -0400

    routing

--- week 7.1/vite-project/src/App.jsx
+++ week 7.1/vite-project/src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes } from 'react-router-dom'
+import { Dashboard } from './pages/Dashboard'
+import { Home } from './pages/Home'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 

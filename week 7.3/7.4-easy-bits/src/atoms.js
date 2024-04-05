010697939527f3c380d4ad9a7401c55dcc96de0c
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-04-05 00:52:16 -0400

    making atoms

--- week 7.3/7.4-easy-bits/src/atoms.js
+++ week 7.3/7.4-easy-bits/src/atoms.js
@@ -2,22 +2,22 @@ import { atom, selector } from "recoil";
 
 export const networkAtom = atom({
     key: "networkAtom",
-    default: 102
+    defaul: 102
 });
 
 export const jobsAtom = atom({
     key: "jobsAtom",
-    default: 0
-});
-
-export const notificationsAtom = atom({
-    key: "notificationsAtom",
-    default: 12
+    defaul: 0
 });
 
 export const messagingAtom = atom({
-    key: "messagingAtom",
-    default: 0
+    key: "networkAtom",
+    defaul: 0
+});
+
+export const notificationsAtom = atom({
+    key: "networkAtom",
+    defaul: 12
 });
 
 export const totalNotificationSelector = selector({

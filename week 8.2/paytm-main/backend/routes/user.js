35b98d728f21e584eeff794c6be1b17a99ebaac0
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-04-08 02:26:12 -0400

    jwt and zod added

--- week 8.2/paytm-main/backend/routes/user.js
+++ week 8.2/paytm-main/backend/routes/user.js
@@ -2,14 +2,29 @@ import express from express;
 import { User } from '../db';
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../config");
+const z = require("zod");
 
 const router = express.Router();
+
+const signUpSchema = z.object({
+    username: z.string(),
+    firstName: z.string(),
+    lastName: z.string(),
+	password: z.string()
+})
 
 router.post('/signup', async (req, res)=>{
     const username = req.body.username;
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
     const password = req.body.password;
+
+    const { success } = signUpSchema.safeParse(req.body);
+    if(!success) {
+        return res.status(411).json({
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
 
     const existingUser = await User.findOne({
         username: username
@@ -21,18 +36,22 @@ router.post('/signup', async (req, res)=>{
         })
     }
 
-    const user = new User({
+    const user = await User.create({
         username,
         password,
         firstName,
         lastName
     })
 
-    user.save();
+    const userId = user._id;
+
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET);
 
     return res.status(200).json({
         message: "User created successfully",
-	    token: "jwt"
+	    token: token
     })
 
 })  

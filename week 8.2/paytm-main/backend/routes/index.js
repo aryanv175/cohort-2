b7556771096fe707f92aa93d1625268f68a7f099
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-04-08 00:14:33 -0400

    router definition

---
+++ week 8.2/paytm-main/backend/routes/index.js
@@ -0,0 +1,15 @@
+import express from express
+const app = express();
+const PORT = 3000;
+
+const router = express.router();
+
+export const apiRouter = router.get('/api/v1', (req, res)=>{
+    res.send("Hello");
+})
+
+app.listen(PORT, (err)=>{
+    if(err) console.log("error");
+    console.log("Server listening on PORT", PORT)
+})
+

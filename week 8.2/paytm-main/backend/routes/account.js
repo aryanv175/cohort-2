ca3a7eb2a5744bd7011e6ad5f98f5001eae6865f
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-04-11 23:50:08 -0400

    making account endpoints

--- week 8.2/paytm-main/backend/routes/account.js
+++ week 8.2/paytm-main/backend/routes/account.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const { Account } = require("../db")
+
+router.get('/balance', (req, res)=> {
+
+})
 
 module.exports = router;
 

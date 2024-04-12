28cf1157d0563623436ae4929758db1b735b0e66
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-04-11 23:33:48 -0400

    account router made

--- week 8.2/paytm-main/backend/routes/index.js
+++ week 8.2/paytm-main/backend/routes/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const userRouter = require("./user.js");
-
+const accountROuter = require("./account.js")
 const router = express.Router();
 
 router.use('/user', userRouter);
+router.use('/account', accountROuter);
 
 module.exports = router;
 

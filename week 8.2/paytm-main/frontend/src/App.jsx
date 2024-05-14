e4d5b86020bc40b38173bbe8e9eb7023226dfe96
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-05-14 12:56:35 +0530

    making the pages

--- week 8.2/paytm-main/frontend/src/App.jsx
+++ week 8.2/paytm-main/frontend/src/App.jsx
@@ -1,17 +1,17 @@
 import {BrowserRouter, Router, Route} from 'react-router-dom';
-import SignUp from "./components/SignUp";
-import SignIn from "./components/SignIn";
-import Dashboard from "./components/Dashboard";
-import SendMoney from "./components/Send";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
+import SendMoney from "./pages/Send";
 
 
 function App() {
 
   return (
     <BrowserRouter>
-      <Router>
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="/signin" element={<Signin />} />
+      <Router> 
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/signin" element={<SignIn/>} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/send" element={<SendMoney />} />
       </Router>
@@ -19,7 +19,7 @@ function App() {
   )
 }
 
-//hey
+
 
 
 export default App

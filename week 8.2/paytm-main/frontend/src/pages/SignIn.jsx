19c2cb35d1e2416e546964679e2fee7aeace9f14
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-05-15 19:14:25 +0530

    added tailwind css

--- week 8.2/paytm-main/frontend/src/pages/SignIn.jsx
+++ week 8.2/paytm-main/frontend/src/pages/SignIn.jsx
@@ -5,13 +5,18 @@ import { SubHeading } from "../components/SubHeading";
 import { BottomWarning } from "../components/BottomWarning";
 
 export const SignIn = () => {
-    return <div>
-        <Heading label={"Sign In"}/>
-        <SubHeading label={"Enter your credentials to access your account"} />
-        <InputBox placeholder="Enter your email" label={"Email"}/>
-        <InputBox placeholder="Enter your password" label={"Password"}/>
-        <div className="pt-4">
-          <Button label={"Sign in"} />
+    return <div className="bg-slate-300 h-screen flex justify-center">
+        <div>
+            <div>
+                <Heading label={"Sign In"}/>
+                <SubHeading label={"Enter your credentials to access your account"} />
+                <InputBox placeholder="Enter your email" label={"Email"}/>
+                <InputBox placeholder="Enter your password" label={"Password"}/>
+                <div className="pt-4">
+                    <Button label={"Sign in"} />
+                </div>
+                <BottomWarning label={"Don't have an account?"} buttonText={"Sign up"} to={"/signup"}/>
+            </div>
         </div>
     </div>
 }

237ef7767ffe2f95ebda4db66bc3eaa77995f2e3
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-05-15 19:15:31 +0530

    added more tw css

--- week 8.2/paytm-main/frontend/src/pages/SignIn.jsx
+++ week 8.2/paytm-main/frontend/src/pages/SignIn.jsx
@@ -6,8 +6,8 @@ import { BottomWarning } from "../components/BottomWarning";
 
 export const SignIn = () => {
     return <div className="bg-slate-300 h-screen flex justify-center">
-        <div>
-            <div>
+        <div className="flex flex-col justify-center">
+            <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
                 <Heading label={"Sign In"}/>
                 <SubHeading label={"Enter your credentials to access your account"} />
                 <InputBox placeholder="Enter your email" label={"Email"}/>

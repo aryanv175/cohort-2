37b7bb382f69aefb7cf03e7d4351e0404cd3bc09
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-05-15 19:06:47 +0530

    making sign in page

---
+++ week 8.2/paytm-main/frontend/src/pages/SignIn.jsx
@@ -0,0 +1,12 @@
+import { Button } from "../components/Button";
+import { InputBox } from "../components/InputBox";
+import { Heading } from "../components/Heading";
+import { SubHeading } from "../components/SubHeading";
+import { BottomWarning } from "../components/BottomWarning";
+
+export const SignIn = () => {
+    return <div>
+        <Heading label={"Sign In"}/>
+        <SubHeading/>
+    </div>
+}
